8e6b240fffef152367db10df38a980dc0205a81c
Kucherenko Sergey <buhanka1994@gmail.com>
2018-09-08 17:20:25 +0300

    Fix branding link

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,9 +8,9 @@ const Header = props => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-info mb-3 py-0">
       <div className="container">
-        <a href="/" className="navbar-brand">
+        <NavLink to="/" className="navbar-brand">
           {branding}
-        </a>
+        </NavLink>
         <button
           className="navbar-toggler"
           type="button"
